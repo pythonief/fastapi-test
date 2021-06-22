eda96870f8830e97d27e1bc3c3c6fe24cff2111f
pythonief <fritzlerilan2@gmail.com>
2021-06-22 14:31:09 -0300

    query parameters learning

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/", description='Some description')
-async def index():
-    return {'data': {'name': 'Ilan'}}
+@app.get("/blog")
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
-# This method needs to be below the about endpoint bcse fastapi read the endpoints in order
 @app.get("/blog/unpublished")
 def unpublished():
+    # This method needs to be below the about endpoint bcse fastapi read the endpoints in order
     return {'data': 'all unpublished blogs'}
 
 
@@ -20,5 +24,5 @@ def about(id: int):
 
 
 @app.get("/blog/{id}/comments")
-def comments(id: int):
+def comments(id: int, limit: int = 10):
     return {'data': {'1', '2'}}
