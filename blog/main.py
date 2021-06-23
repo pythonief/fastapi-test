f4e0d7cd8deb43acd073648822c98dbe450d5096
pythonief <fritzlerilan2@gmail.com>
2021-06-23 01:45:57 -0300

    response models

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, status, Response
 from fastapi.exceptions import HTTPException
 from fastapi.params import Depends
@@ -53,13 +54,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
